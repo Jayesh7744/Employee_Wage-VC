047ac6aac786e88906e7699bff1e0fd0220dbcc2
Jayesh7744 <Jayeshs7744@gmail.com>
2022-05-26 15:17:19 +0530

    Added: Present or Absent

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,1 +1,14 @@
+#!/bin/bash -x
 echo "Welcome"
+isPresent=0
+randomCheck=$(( RANDOM%2 ))
+
+
+   	if (( $isPresent - $randomCheck ))
+           then
+              (( $randomCheck == $isPresent ))
+
+              echo "Present"
+	else
+              echo "Absent"
+        fi
