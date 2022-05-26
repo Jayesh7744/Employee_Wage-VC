80aa4b94dc53379b5b603bee6dd73f341c1bd08b
Jayesh7744 <Jayeshs7744@gmail.com>
2022-05-26 15:28:17 +0530

    Added: Daily Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -x
-echo "Welcome"
+echo "Welcome to Employee Wage Computation Program"
 isPresent=0
 randomCheck=$(( RANDOM%2 ))
 
@@ -12,3 +12,16 @@ randomCheck=$(( RANDOM%2 ))
 	else
               echo "Absent"
         fi
+
+isPresent=1;
+randomCheck=$(( RANDOM%2 ));
+
+if(( $isPresent - $randomCheck ))
+ then
+        empRatePerHr=20;
+        empHrs=8;
+        salery=$(( empHrs * empRatePerHr ))
+
+else
+        salery=0
+fi
